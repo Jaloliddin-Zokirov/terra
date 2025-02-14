f52af5a4283f03f80776aedff85587e14c50c12c
Shahriyor Adhamov <nasirovairoda41@gmail.com>
2025-02-14 21:07:14 +0500

    style: add background to youtube text area

--- src/Pages/Home/components/CarVideoYoutube.jsx
+++ src/Pages/Home/components/CarVideoYoutube.jsx
@@ -1,6 +1,6 @@
 const CarVideoYoutube = () => {
   return (
-    <div className="bg-[#1E1F27] text-white py-12 px-6 md:px-12 flex flex-col lg:flex-row items-center gap-12">
+    <div className="bg-[#1E1F27] relative text-white py-12 px-6 md:px-12 flex flex-col lg:flex-row items-center gap-12">
 
       <div className="lg:w-3/5 w-full">
         <iframe
@@ -14,6 +14,7 @@ const CarVideoYoutube = () => {
       </div>
         
       <div className="lg:w-2/5 w-full relative">
+        <div className="absolute right-0 opacity-40 w-full md:w-1/2 h-full  bg-[url('/assets/halifa.png')] bg-cover" />
         <div className="relative z-10 p-6">
           <h2 className="text-3xl font-bold mb-6">SPORTS CAR RENTAL DUBAI</h2>
           <p className="text-gray-300 mb-8 text-lg">
