37388f1195a0075733c018603e72965d1d102038
Acer <abduhalilismoilov560@gmail.com>
2025-02-15 14:52:19 +0500

    feature/new-luxury-top-section-add

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,10 +4,12 @@ import { LuxuryCar } from "../../Components/LuxuryCar/LuxuryCar";
 import Brands from "../../Components/Brands/Brands";
 import Faq from "../Faq/Faq";
 import Services from "../../Components/Services/Services";
+import { TopLuxury } from "../../Components/TopLuxury/TopLuxury";
 
 const Home = () => {
   return (
     <section className="bg-[#111219] relative">
+      <TopLuxury />
       <Brands />
       <Services />
       <LuxuryCar />
