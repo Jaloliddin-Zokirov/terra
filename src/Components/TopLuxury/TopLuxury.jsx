f5c071375c23f16e616bf9441fd066640158e2f8
Acer <abduhalilismoilov560@gmail.com>
2025-02-17 23:58:46 +0500

    top-luxury-bugs

--- src/Components/TopLuxury/TopLuxury.jsx
+++ src/Components/TopLuxury/TopLuxury.jsx
@@ -1,17 +1,35 @@
 import { Link } from "react-router-dom";
 import { GrCaretNext } from "react-icons/gr";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
+import NextImg from "../../../public/assets/next-img.svg";
+
 import TopCar1 from "../../../public/assets/top-car1.png";
 import TopCar2 from "../../../public/assets/top-car2.png";
 import TopCar3 from "../../../public/assets/top-car3.png";
 import TopCar4 from "../../../public/assets/top-car4.png";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 export function TopLuxury() {
+  const images = [TopCar1, TopCar2, TopCar3, TopCar4];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const changeImage = (newIndex) => {
+    setAnimating(true);
+    setTimeout(() => {
+      setCurrentIndex(newIndex);
+      setAnimating(false);
+    }, 500); // Adjust duration for smoother effect
+  };
+
+  const nextImage = () => {
+    changeImage((currentIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    changeImage(currentIndex === 0 ? images.length - 1 : currentIndex - 1);
+  };
+
   const { lang } = useSelector((state) => state.lang);
   return (
     <section id="hero">
@@ -40,49 +58,51 @@ export function TopLuxury() {
             <GrCaretNext fill="#ffff" />
           </Link>
         </article>
+
+        {/* bg-gradient-to-t from-[#343744] via-[#343744] to-transparent */}
       </div>
 
-      <div className="relative bg-gradient-to-t from-[#343744] via-[#343744] to-transparent">
-        <Swiper
-          watchSlidesProgress={true}
-          loop={true}
-          navigation={true}
-          effect={"coverflow"}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={3}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
-          pagination={true}
-          modules={[EffectCoverflow, Pagination, Navigation]}
-          className="mySwiper mt-[100px]"
-        >
-          <SwiperSlide>
-            <Link to="/cars">
-              <img className="img w-full h-full" src={TopCar1} alt="img" />
+      <div className="relative  bg-gradient-to-t from-[#343744] via-[#343744] to-transparent">
+        <div className="flex flex-col items-center gap-4 p-12     mx-auto mt-[100px]">
+          <div className="md:flex block items-center justify-center mx-auto gap-4 w-full">
+            <img
+              src={images[(currentIndex + images.length - 1) % images.length]}
+              alt="previous"
+              className={`w-1/4 hidden md:block opacity-50 transition-all duration-500 ${
+                animating ? "opacity-30" : ""
+              }`}
+            />
+            <Link
+              className={`w-1/2 transition-all duration-500 transform ${
+                animating ? "scale-90 opacity-70" : "scale-100"
+              } hover:scale-105`}
+              to="/cars"
+            >
+              <img src={images[currentIndex]} alt="current" />
             </Link>
-          </SwiperSlide>
-          <SwiperSlide>
-            <Link to="/cars">
-              <img className="img w-full h-full" src={TopCar2} alt="img" />
-            </Link>
-          </SwiperSlide>
-          <SwiperSlide>
-            <Link to="/cars">
-              <img className="img w-full h-full" src={TopCar3} alt="img" />
-            </Link>
-          </SwiperSlide>
-          <SwiperSlide>
-            <Link to="/cars">
-              <img className="img w-full h-full" src={TopCar4} alt="img" />
-            </Link>
-          </SwiperSlide>
-        </Swiper>
+            <img
+              src={images[(currentIndex + 1) % images.length]}
+              alt="next"
+              className={`w-1/4 hidden md:block opacity-50 transition-all duration-500 ${
+                animating ? "opacity-30" : ""
+              }`}
+            />
+          </div>
+          <div className="flex gap-12 md:gap-96">
+            <button
+              onClick={prevImage}
+              className="px-4 py-2 text-gray-600  rotate-180 cursor-pointer hover:scale-120 transition-all duration-300 hover:opacity-45 opacity-70 "
+            >
+              <img className="md:w-12 w-5 md:h-8 h-3" src={NextImg} alt="img" />
+            </button>
+            <button
+              onClick={nextImage}
+              className="px-4 py-2  text-gray-600 cursor-pointer hover:scale-120 transition-all duration-300 hover:opacity-45 opacity-70"
+            >
+              <img className="md:w-12 w-5 md:h-8 h-3" src={NextImg} alt="img" />
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   );
